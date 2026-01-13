1cf23667546c764250db7df9c26bfa960a83a87e
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2026-01-13 19:45:02 +0900

    chore: remove comment

--- plugin/comment-translate.lua
+++ plugin/comment-translate.lua
@@ -2,6 +2,3 @@ if vim.g.loaded_comment_translate == 1 then
   return
 end
 vim.g.loaded_comment_translate = 1
-
--- Expose global setup if desired, or rely on require('comment-translate').setup
--- Standard convention is usually requiring the lua module.
